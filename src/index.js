b46360bb997218766d65f123140b95de22c57a82
veecee424 <valentine424@yahoo.com>
2021-09-28 18:11:26 +0100

    added more styling and fine tuning

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Card.css';
 import App from './App'
+import './index.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
